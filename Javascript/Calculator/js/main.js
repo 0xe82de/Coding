1915b984ea06247149730a8ef0b6f403face1e30
0xe82de <0xe82de@gmail.com>
2020-08-30 00:09:58 +0900

    calculator 1.0 update

--- Javascript/Calculator/js/main.js
+++ Javascript/Calculator/js/main.js
@@ -1,65 +1,123 @@
 "use strict";
 
-let checkOp;
+const btns = document.getElementById("jsBtns");
+const printResult = document.getElementById("jsPrintResult");
+const printTemp = document.getElementById("jsPrintTemp");
+let checkOp = 0;
 
-function setValue(value) {
-  document.getElementById("js-result").innerHTML = value;
-}
-
-function isNaN(value) {
-  if (Number.isNaN(Number(value))) return 1;
-  return 0;
-}
-
-function isNum(value) {
-  if (isNaN(value)) {
-    return;
-    // 수식 함수 추가
-  }
-
-  setValue(value);
-}
-
-function isZero() {
-  return document.getElementById("js-result").innerHTML;
-}
-
-function printNum(value) {
-  let checkZero = isZero();
-
-  if (checkZero !== "0" && checkOp === 0) {
-    value = checkZero + value;
-    document.getElementById("js-result").innerHTML = value;
-  } else {
-    document.getElementById("js-result").innerHTML = value;
-  }
-
-  checkOp = 0;
-
-  return;
-}
-
-function printOP(op) {
-  let tmpResult;
-
-  if (document.getElementById("js-tmpResult").innerHTML === "0") {
-    tmpResult = document.getElementById("js-result").innerHTML;
-    document.getElementById("js-tmpResult").innerHTML = tmpResult + " " + op;
-  } else {
-    tmpResult = document.getElementById("js-tmpResult").innerHTML;
-    tmpResult =
-      tmpResult + " " + document.getElementById("js-result").innerHTML;
-    document.getElementById("js-tmpResult").innerHTML = tmpResult + " " + op;
-  }
-
-  return;
-}
-
-function whatKey(key) {
-  if (checkOp === undefined) {
+function printNum(key) {
+  if (checkOp === 1) {
     checkOp = 0;
+    printResult.innerHTML = "0";
+  }
+  if (printResult.innerHTML === "0") {
+    printResult.innerHTML = key;
+  } else {
+    printResult.innerHTML = printResult.innerHTML + key;
+  }
+}
+
+function changeSign() {
+  if (printResult.innerHTML[0] === "-") {
+    printResult.innerHTML = printResult.innerHTML.slice(1);
+  } else {
+    printResult.innerHTML = "-" + printResult.innerHTML;
+  }
+}
+
+function addDot() {
+  if (printResult.innerHTML.indexOf(".") === -1) {
+    printResult.innerHTML = printResult.innerHTML + ".";
+  }
+}
+
+function square() {
+  printResult.innerHTML = printResult.innerHTML * printResult.innerHTML;
+}
+
+function clear() {
+  printResult.innerHTML = "0";
+  printTemp.innerHTML = "0";
+}
+
+function addOp(key) {
+  if (checkOp === 1) {
+    if (printTemp.innerHTML == "0") {
+      printTemp.innerHTML = printResult.innerHTML + key;
+    } else {
+      printTemp.innerHTML = printTemp.innerHTML.slice(0, -1) + key;
+    }
+  } else if (printTemp.innerHTML === "0") {
+    printTemp.innerHTML = printResult.innerHTML + key;
+  } else {
+    printTemp.innerHTML = printTemp.innerHTML + printResult.innerHTML + key;
+  }
+}
+
+function getEx() {
+  return printTemp.innerHTML + printResult.innerHTML;
+}
+
+function calculate(ex) {
+  let tmpNums = ex.split(/÷|×|－|＋/);
+  let tmpResult = [];
+  let count = 0;
+  const lengthNums = tmpNums.length;
+  const lengthOps = lengthNums - 1;
+  let lengthTmp = ex.length;
+  let countOp = 0;
+  let tmp = 0;
+
+  // 피연산자 저장
+  for (let i = 0; i < lengthNums; i++) {
+    tmpResult[2 * i] = Number(tmpNums[i]);
   }
 
+  // 연산자 저장
+  for (let i = 0; i < lengthOps; i++) {
+    for (let j = count; j < ex.length; j++) {
+      if (ex[j] === "÷" || ex[j] === "×" || ex[j] === "－" || ex[j] === "＋") {
+        tmpResult[2 * i + 1] = ex[j];
+        count = j + 1;
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < lengthOps; i++) {
+    countOp = tmpResult.indexOf("÷");
+    if (countOp != -1) {
+      tmp = tmpResult[countOp - 1] / tmpResult[countOp + 1];
+      tmpResult[countOp - 1] = tmp;
+      tmpResult.splice(countOp, 2);
+    }
+
+    countOp = tmpResult.indexOf("×");
+    if (countOp != -1) {
+      tmp = tmpResult[countOp - 1] * tmpResult[countOp + 1];
+      tmpResult[countOp - 1] = tmp;
+      tmpResult.splice(countOp, 2);
+    }
+
+    countOp = tmpResult.indexOf("－");
+    if (countOp != -1) {
+      tmp = tmpResult[countOp - 1] - tmpResult[countOp + 1];
+      tmpResult[countOp - 1] = tmp;
+      tmpResult.splice(countOp, 2);
+    }
+
+    countOp = tmpResult.indexOf("＋");
+    if (countOp != -1) {
+      tmp = tmpResult[countOp - 1] + tmpResult[countOp + 1];
+      tmpResult[countOp - 1] = tmp;
+      tmpResult.splice(countOp, 2);
+    }
+  }
+
+  printResult.innerHTML = tmpResult[0];
+}
+
+function executeFnc(key) {
   switch (key) {
     case "9":
     case "8":
@@ -71,23 +129,43 @@ function whatKey(key) {
     case "2":
     case "1":
     case "0":
-      // 숫자 ㅋㅋㅋ
       printNum(key);
       break;
-    case "/":
-    case "X":
-    case "-":
-    case "+":
-      checkOp = 1;
-      printOP(key);
+    case "＋/－":
+      changeSign();
+      break;
+    case ".":
+      addDot();
+      break;
+    case "×<sup>2</sup>":
+      square();
       break;
     case "C":
       clear();
+      checkOp = 0;
       break;
-    case "=":
-      compute();
+    case "÷":
+    case "×":
+    case "－":
+    case "＋":
+      addOp(key);
+      checkOp = 1;
+      break;
+    case "＝":
+      let ex = getEx();
+      calculate(ex);
+      // checkOp = 1;
+      printTemp.innerHTML = "0";
       break;
   }
+}
 
-  return;
+function getKey(event) {
+  let key = event.srcElement.innerHTML;
+  executeFnc(key);
+}
+
+if (btns) {
+  let key = btns.addEventListener("click", getKey);
+  // console.log(key);
 }
